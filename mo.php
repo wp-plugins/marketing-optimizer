82385c878706db6e97466389a71c09e4339a6cad
activeim <activeim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-18 16:41:51 +0000

    Updated gravity forms class
    git-svn-id: https://plugins.svn.wordpress.org/marketing-optimizer/trunk@774776 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mo.php
+++ mo.php
@@ -1,17 +1,17 @@
 <?php
 /*
  * Plugin Name: Marketing Optimizer
- *  Plugin URI: http://www.marketingoptimizer.com 
+ *  Plugin URI: http://www.marketingoptimizer.com/wordpress/ 
  *  Description: A plugin to integrate with Marketing Optimizer and perform A/B testing experiments on your wordpress pages. 
- *  Version: 20130918
- *  Author: Stephen R. Croskey, steve@activeinternetmarketing.com 
- *  Author URI: http://www.activeinternetmarketing.com
+ *  Version: 20130918.1
+ *  Author: Marketing Optimizer, customercare@marketingoptimizer.com 
+ *  Author URI: http://www.marketingoptimizer.com/
  */
 ?>
 <?php
 // some definition we will use
 define ( 'MO_PUGIN_NAME', 'Marketing Optimizer' );
-define ( 'MO_CURRENT_VERSION', '20130918' );
+define ( 'MO_CURRENT_VERSION', '20130918.1' );
 define ( 'MO_CURRENT_BUILD', '1' );
 define ( 'MO_PLUGIN_DIRECTORY', 'marketing-optimizer');
 define ( 'MO_LOGPATH', str_replace ( '\\', '/', WP_CONTENT_DIR ) . '/mo-logs/' );
